44ab24ad3cfb44354ef2ee74e868986019110e89
unknown <p.abdoon@gmail.com>
2018-04-03 15:22:53 +0200

    Add App classe

---
+++ src/tp/greenComputing/Classes/App.java
@@ -0,0 +1,64 @@
+package tp.greenComputing.Classes;
+
+public class App {
+
+	private String nomApp;
+	private double consoApp;
+	private boolean useInternet;
+	private boolean useBleuthoot;
+	private int luminosite;
+	private int dureeMin;
+	
+	public App() {
+		
+	}
+
+	public String getNomApp() {
+		return nomApp;
+	}
+
+	public void setNomApp(String nomApp) {
+		this.nomApp = nomApp;
+	}
+
+	public double getConsoApp() {
+		return consoApp;
+	}
+
+	public void setConsoApp(double consoApp) {
+		this.consoApp = consoApp;
+	}
+
+	public boolean isUseInternet() {
+		return useInternet;
+	}
+
+	public void setUseInternet(boolean useInternet) {
+		this.useInternet = useInternet;
+	}
+
+	public boolean isUseBleuthoot() {
+		return useBleuthoot;
+	}
+
+	public void setUseBleuthoot(boolean useBleuthoot) {
+		this.useBleuthoot = useBleuthoot;
+	}
+
+	public int getLuminosite() {
+		return luminosite;
+	}
+
+	public void setLuminosite(int luminosite) {
+		this.luminosite = luminosite;
+	}
+
+	public int getDureeMin() {
+		return dureeMin;
+	}
+
+	public void setDureeMin(int dureeMin) {
+		this.dureeMin = dureeMin;
+	}
+	
+}
