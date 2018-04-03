56dc3b3f19fb3790317ba784eaf85c38c858f77f
unknown <p.abdoon@gmail.com>
2018-04-03 14:27:18 +0200

    Create class Materiel

---
+++ src/tp/greenComputing/materiel/Materiel.java
@@ -0,0 +1,29 @@
+package tp.greenComputing.materiel;
+
+public class Materiel {
+
+	private String materielName;
+	private boolean isActive;
+	
+	public Materiel() {
+		
+	}
+
+	public String getMaterielName() {
+		return materielName;
+	}
+
+	public void setMaterielName(String materielName) {
+		this.materielName = materielName;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+}
