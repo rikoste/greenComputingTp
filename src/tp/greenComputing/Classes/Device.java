c0e671c3933eaa508ce51dd4d1fa7192ed025c39
unknown <p.abdoon@gmail.com>
2018-04-03 20:49:01 +0200

    Device

--- src/tp/greenComputing/Classes/Device.java
+++ src/tp/greenComputing/Classes/Device.java
@@ -17,6 +17,7 @@ public class Device {
 	
 	
 	private ArrayList<App> apps = new ArrayList<App>();
+	private ArrayList<Sequ> sequences = new ArrayList<Sequ>();
 	
 	
 	public Device() {
@@ -68,7 +69,7 @@ public class Device {
 
 	public void initApp() {
 		App facebook = new App("FaceBook", 0.15, true, false, 30, 15);
-		App chrome = new App("Google Chrome", 0.17, true, false, 30, 15);
+		App chrome = new App("Chrome", 0.17, true, false, 30, 15);
 		App whatsapp = new App("WhatsApp", 0.17, true, false, 25, 10);
 		App twitter = new App("Twitter", 0.16, true, false, 25, 15);
 		App instagram = new App("Instagram", 0.16, true, false, 30, 15);
@@ -78,7 +79,86 @@ public class Device {
 		this.apps.add(whatsapp);
 		this.apps.add(twitter);
 		this.apps.add(instagram);
+		
+		
+		//-----------Sequences---------//
+		
+		// Facbook, Twitter, WhatsApp, Chrome,Instagram
+		
+			//Facebook, Twitter, Instagram
+			// Facbook, Twitter,Chrome
+			//Facbook,  Chrome,Instagram
+			//Facebook, Twitter,Instagram,
+			
+			
+			//Facebook=4
+			//Twitter=3
+			//Whatsup=1
+			//Chrome=2
+			//Instagram=2
+			
+			//Facebook-Twitter=3
+			//Facebook-Chrome=2
+			//Facebook-Instagram=2
+			//Twitter-Instagram=2
+			
+			//Facebook-twitter-instagram=2
+		
+		
+		Sequ facebookSequ = new Sequ("facebook",4,"twitter","twitter");
+		Sequ twitterSequ = new Sequ("twitter",3,"Instagram","Instagram");
+		Sequ whatsupSequ = new Sequ("whatsup",1,null,null);
+		Sequ chromeSequ = new Sequ("chrome",2,"instagram",null);
+		Sequ instagramSequ = new Sequ("instagram",2,null,null);
+		
+		
+		this.sequences.add(facebookSequ);
+		this.sequences.add(twitterSequ);
+		this.sequences.add(whatsupSequ);
+		this.sequences.add(chromeSequ);
+		this.sequences.add(instagramSequ);
+		
+		
+		
 	}
+	
+	
+	//Fonction qui retourne de la fréquence 0.
+	//si elle retourne 0 cela veut dire que l'app en paramètre n'a pas de sequence	
+	public int getSequence0(String appNom) {			
+		for (Sequ sequence : this.sequences) {
+		     if(sequence.getApp().equals(appNom)) {
+		    	 return sequence.getFrequence0();
+		    }
+		}
+		
+		return 0;
+	}
+	
+	
+	
+	public App getSequence(String appNom, int i) {
+		
+		if(i==1){
+			for (Sequ sequence : this.sequences) {
+			     if(sequence.getApp().equals(appNom)) {
+			    	 return getAppByName(sequence.getFrequence1());
+			    }
+			}
+		}else {
+			for (Sequ sequence : this.sequences) {
+			     if(sequence.getApp().equals(appNom)) {
+			    	 return getAppByName(sequence.getFrequence2());
+			    }
+			}
+		}
+		
+		return null;
+	}
+	
+	
+	
+	
 	
 	public App getAppByName(String nomApp) {
 		
