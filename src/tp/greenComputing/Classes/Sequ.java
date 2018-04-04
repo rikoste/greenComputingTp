477be1c04e8701a07f4282b05c0cb828a6af572d
unknown <p.abdoon@gmail.com>
2018-04-04 09:06:03 +0200

    Squence

---
+++ src/tp/greenComputing/Classes/Sequ.java
@@ -0,0 +1,89 @@
+package tp.greenComputing.Classes;
+
+public class Sequ {
+
+	
+	
+	
+		
+	private String app;
+	private int frequence0;
+	private String frequence1;
+	private String frequence2;
+	
+	
+		
+
+	public Sequ(String app, int frequence0, String frequence1, String frequence2) {
+		super();
+		this.app = app;
+		this.frequence0 = frequence0;
+		this.frequence1 = frequence1;
+		this.frequence2 = frequence2;
+	}
+
+
+
+
+	public void sequ() {}
+
+
+
+
+	public String getApp() {
+		return app;
+	}
+
+
+
+
+	public void setApp(String app) {
+		this.app = app;
+	}
+
+
+
+
+	public int getFrequence0() {
+		return frequence0;
+	}
+
+
+
+
+	public void setFrequence0(int frequence0) {
+		this.frequence0 = frequence0;
+	}
+
+
+
+
+	public String getFrequence1() {
+		return frequence1;
+	}
+
+
+
+
+	public void setFrequence1(String frequence1) {
+		this.frequence1 = frequence1;
+	}
+
+
+
+
+	public String getFrequence2() {
+		return frequence2;
+	}
+
+
+
+
+	public void setFrequence2(String frequence2) {
+		this.frequence2 = frequence2;
+	}
+	
+	
+	
+	
+}
